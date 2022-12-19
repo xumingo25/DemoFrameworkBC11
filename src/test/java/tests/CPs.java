4daca731865463e371a56ff67b1c53c6103822d9
Domingo Saavedra <domingo.saavedra@tsoftglobal.com>
2022-12-19 13:07:46 -0300

    Se actualiza la llamada a los datos de prueba del proyecto. Invocación via DataDriven

--- src/test/java/tests/CPs.java
+++ src/test/java/tests/CPs.java
@@ -7,6 +7,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.RegisterPage;
+import utils.DataDriven;
+
+import java.util.ArrayList;
 
 public class CPs {
     //Atributos
@@ -18,8 +21,11 @@ public class CPs {
     private String urlDriver = System.getProperty("user.dir")+"\\src\\test\\resources\\drivers\\chromedriver.exe";
     private String url = "https://www.spotify.com/";
 
+    private ArrayList<String> data; //null
+
     @BeforeMethod
     public void preparacionTests(){
+        data = new ArrayList<String>();  //Array de tamaño 0
         homePage = new HomePage(driver); //Se crea la page del home
         homePage.conexionBrowser(browser,propertyDriver,urlDriver); //Se conecta el driver de chrome
         registerPage = new RegisterPage(homePage.getDriver()); //se crea la page de registro
@@ -30,16 +36,20 @@ public class CPs {
 
     @Test
     public void CP001_Registro_Fallido_Captcha_en_blanco(){
+        data = DataDriven.getData("CP001_Registro_Fallido_Captcha_en_blanco");
         homePage.irARegistrarte();
-        registerPage.completarFormularioRegistro("domingo.saavedra@tsoftglobal.com", "domingo.saavedra@tsoftglobal.com", "123454321", "Pobre Domingo", "31", "Abril", "1990");
-        Assert.assertEquals("Confirma que no eres un robot.",registerPage.obtenerErrorCaptchaVacio());
+        registerPage.completarFormularioRegistro(
+                data.get(1), data.get(2), data.get(3), data.get(4), data.get(5), data.get(6), data.get(7));
+        Assert.assertEquals(data.get(8),registerPage.obtenerErrorCaptchaVacio());
     }
 
     @Test
     public void CP002_Registro_Fallido_Password_Corta(){
+        data = DataDriven.getData("CP002_Registro_Fallido_Password_Corta");
         homePage.irARegistrarte();
-        registerPage.completarFormularioRegistro("", "", "1", "", "", "Mayo", "");
-        Assert.assertEquals("Tu contraseña es demasiado corta.",registerPage.obtenerErrorPasswordCorta());
+        registerPage.completarFormularioRegistro
+                (data.get(1), data.get(2), data.get(3), data.get(4), data.get(5), data.get(6), data.get(7));
+        Assert.assertEquals(data.get(8),registerPage.obtenerErrorPasswordCorta());
     }
 
     @AfterMethod
