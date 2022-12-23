2fbb636089d6aec822f15ac09048de0dc10c172f
JeniferDenise <espeche.jenifer@tsoftglobal.com>
2022-12-23 19:04:51 -0300

    prueba3

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -3,7 +3,6 @@ package Pages;
 import org.openqa.selenium.By;
 
 public class HomePage {
-    By locatorBtnVuelos = By.xpath()
-    By locatorBtnVuelo34 = By.xpath()
-        //feliz navidad codeando xD
+    //borre prueba3
+
 }
