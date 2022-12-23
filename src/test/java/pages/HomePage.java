86cc76b8253dd16dbfd8d018623e225af0721cc0
aaronbarra <aaron.barra@tsoftglobal.com>
2022-12-23 03:53:13 -0300

    Se añade caso de prueba CP007_BusquedaTrenes_IdaVuelta, drivers, HomePage y TrenPage

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.BaseClass;
+
+public class HomePage extends BaseClass {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    //Localizadores
+
+    By locatorBtnAceptarCookies = By.xpath("//button[contains(text(),'Aceptar todo')]");
+    By locatorBtnTrenes = By.xpath("//a[@title=\"Trenes\"]");
+    By locatorBtnVuelos = By.xpath("//a[@title=\"Vuelos\"]");
+    By locatorBtnVuelo_Hotel= By.xpath("//a[@title=\"Vuelo + Hotel\"]");
+    By locatorBtnViajes= By.xpath("//a[@title=\"Viajes\"]");
+    By locatorBtnTren_Hotel= By.xpath("//a[@title=\"Tren + Hotel\"]");
+    By locatorBtnHoteles= By.xpath("//a[@title=\"Hoteles\"]");
+
+    //Metodos
+
+    public void aceptarCookies(){
+        esperarXSegundos(4000);
+        click(esperarAElementoWeb(locatorBtnAceptarCookies));
+        esperarXSegundos(4000);
+    }
+
+    public void irATrenes(){
+        click(esperarAElementoWeb(locatorBtnTrenes));
+    }
+
+    public void irAVuelos(){
+        click(esperarAElementoWeb(locatorBtnVuelos));
+    }
+
+    public void irAVuelo_Hotel(){
+        click(esperarAElementoWeb(locatorBtnVuelo_Hotel));
+    }
+
+    public void irAViajes(){
+        click(esperarAElementoWeb(locatorBtnViajes));
+    }
+
+    public void irATren_Hotel(){
+        click(esperarAElementoWeb(locatorBtnTren_Hotel));
+    }
+
+    public void irAHoteles(){
+        click(esperarAElementoWeb(locatorBtnHoteles));
+    }
+
+
+
+
+}
---
+++ src/test/java/tests/CasosDePrueba.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.TrenPage;
+import utils.PropertiesDriven;
+
+import java.util.ArrayList;
+
+public class CasosDePrueba {
+    private HomePage homePage;
+    private TrenPage trenPage;
+    private WebDriver driver;
+    private final String browser = PropertiesDriven.getProperty("browser");
+    private final String propertyDriver = PropertiesDriven.getProperty("propertyDriver");
+    private final String urlDriver = PropertiesDriven.getProperty("urlDriver");
+    private final String url = PropertiesDriven.getProperty("url");
+
+    private ArrayList<String> data; // null
+    private ArrayList<String> data2;
+
+    @BeforeMethod
+    public void preparacionTests(){
+        data= new ArrayList<String>(); //Array de tamaño 0
+        data2= new ArrayList<String>(); //Segundo Array para evitar conflictos con data
+        homePage = new HomePage(driver);
+        homePage.conexionBrowser(browser,propertyDriver,urlDriver);
+        homePage.cargarPagina(url);
+        homePage.maximizarBrowser();
+        trenPage = new TrenPage(homePage.getDriver());
+    }
+
+    @AfterMethod
+    public void posTests(){;
+    }
+
+    @Test
+    public void CP007_BusquedaTrenes_IdaVuelta() {
+        homePage.aceptarCookies();
+        homePage.irATrenes();
+        trenPage.completarBusqueda("Madrid", "Barcelona");
+
+
+    }
+}
