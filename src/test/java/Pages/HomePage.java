19ee5632e6ad3eee6b65f9e52a2c3f3bbe3cc103
Ignacio Marrero <ignacio.marrero@tsoftglobal.com>
2022-12-24 01:35:12 -0300

    Se agregan localizadores. Se crea Metodo CompletarLogin. Se crea metodo irAVuelos

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,21 +1,37 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.BaseClass;
 
-public class HomePage {
-    //borre prueba3
+public class HomePage extends BaseClass {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
 
+    By locatorBtnLogin = By.xpath("//button/p" );
+    By locatorInputEmailLogin = By.name("email");
+    By locatorInputPasswordLogin = By.name("password");
+    By locatorSubmitLogin = By.xpath("//button[contains(text(),'Iniciar sesión')]");
+    By locatorBtnVuelos = By.xpath("//a[@title='Vuelos']" );
+    /*
+    LOCALIZADORES
+    ----------------------------------------------
+    By locatorBtnHoteles = By.xpath("//a[@title='Hoteles']" );
+    By locatorBtnTrenes = By.xpath("//a[@title='Trenes']" );
+    By locatorBtnTrenMasHotel = By.xpath("//a[@title='Tren + Hotel']" );
+    */
 
-    //GFDHSDF
-
-    //asd
-
-    //asddsdd
-
-
-    //a pedazos
-
-
-    //fkjslkdfjas
+public void irALogin () {
+    click(esperarAElementoWeb(locatorBtnLogin));
+}
+public void irAVuelos () {
+    click(esperarAElementoWeb(locatorBtnVuelos));
+    }
+public void CompletarLogin (String correo, String password){
+    agregarTexto(esperarAElementoWeb(locatorInputEmailLogin),correo);
+    agregarTexto(esperarAElementoWeb(locatorInputPasswordLogin),password);
+    click(esperarAElementoWeb(locatorSubmitLogin));
+}
 
 }
